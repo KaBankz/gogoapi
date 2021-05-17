e7c61a9a80799024cb71939e3d1fb88cdcc70c15
KaBankz <xdeathhackzx@gmail.com>
2021-05-17 14:36:52 -0400

    :lock: Typecheck page query

--- src/middleware/queryValidator.ts
+++ src/middleware/queryValidator.ts
@@ -1,9 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 
 function queryValidator(req: Request, _res: Response, next: NextFunction) {
-  // TODO Make sure page is a number
   const { page, query } = req.query;
-  if (page && typeof page !== 'string') next(new Error('Invalid page'));
+  if (page && (typeof page !== 'string' || isNaN(+page))) next(new Error('Invalid page'));
   if (query && typeof query !== 'string') next(new Error('Invalid query'));
   next();
 }
