fbeb728120fae0f9540b60e4c6aa2ca91c255d30
KaBankz <xdeathhackzx@gmail.com>
2021-05-17 16:16:50 -0400

    :fire: Remove popular scrper reference

--- src/scrapers/index.ts
+++ src/scrapers/index.ts
@@ -1,6 +1,5 @@
 import search from './search';
-import popular from './popular';
 import recent from './recent';
 import common from './common';
 
-export { search, popular, recent, common };
+export { search, recent, common };
