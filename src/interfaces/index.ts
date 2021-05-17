837b8e943184fb1cbe30eb4391dcf30bcffab59c
KaBankz <xdeathhackzx@gmail.com>
2021-05-17 16:14:42 -0400

    :fire: Remove popular interface reference

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,10 +1,9 @@
 import Search from './search';
-import Popular from './popular';
 import Recent from './recent';
 import Common from './common';
 import Paths from './paths';
 import Queries from './queries';
 
-export { Search, Popular, Recent, Common, Paths, Queries };
+export { Search, Recent, Common, Paths, Queries };
 
 // TODO: Refactor and move interfaces and declarations to src/@types
